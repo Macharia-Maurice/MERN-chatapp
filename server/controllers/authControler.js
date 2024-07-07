ff13125cec7af9d67d1e839b05abf4e7f79d7af3
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-07 14:17:58 +0300

    ✨ add authcontroller

---
+++ server/controllers/authControler.js
@@ -0,0 +1,11 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const user = require('../models/userModel')
+const createError = require('../utils/appError');
+
+
+// register user
+exports.signup = async (req, res, next)=>{};
+
+
+exports.login = async (req, res, next)=>{};
