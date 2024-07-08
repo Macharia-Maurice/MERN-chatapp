aebf20c11bf933c1cea0f7ff9227d5b31170cc32
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-08 22:59:08 +0300

    make register page modular

--- client/src/components/forms/RegisterForm.jsx
+++ client/src/components/forms/RegisterForm.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import useRegister from "@/hooks/RegisterHook";
+import AuthForm from "../wrappers/AuthForm";
+import FormInput from "../wrappers/FormInput";
 
 const RegisterForm = () => {
   return (
-    <div>RegisterForm</div>
+    <AuthForm
+      title={"Register"}
+      subtitle={"Welcome to the Community!"}
+      submitBtnTxt={"Register"}
+      hook={useRegister}
+    >
+
+      <FormInput
+        name={"first_name"}
+        label={"First Name"}
+        type={"text"}
+      />
+
+      <FormInput
+        name={"last_name"}
+        label={"Last Name"}
+        type={"text"}
+      />
+
+      <FormInput
+        name={"email"}
+        label={"Email"}
+        type={"email"}
+      />
+
+      <FormInput
+        name={"password"}
+        label={"Password"}
+        type={"password"}
+      />
+
+      <FormInput
+        name={"confirm_password"}
+        label={"Confirm Password"}
+        type={"password"}
+      />
+
+    </AuthForm>
   )
 }
 
