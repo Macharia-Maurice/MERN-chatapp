bd2510fddc879d1639e4b1441b04aaa453745414
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-08 23:00:41 +0300

    add register hook

---
+++ client/src/hooks/RegisterHook.jsx
@@ -0,0 +1,65 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterSchema } from "@/schema/RegisterSchema";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+
+const useRegister = () => {
+    const form = useForm({
+        resolver: zodResolver(RegisterSchema),
+        defaultValues: {
+            first_name: "",
+            last_name: "",
+            email: "",
+            password: "",
+            confirm_password: ""
+        },
+    });
+
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (data) => {
+        const {
+            first_name,
+            last_name,
+            email,
+            password,
+            re_password
+        } = data
+
+        try {
+            setIsLoading(true);
+            setError(null);
+            const response = await axios.post('http://localhost:2000/api/auth/register', {
+                first_name,
+                last_name,
+                email,
+                password,
+                re_password
+            })
+            console.log(response.data)
+            navigate('/dashboard')
+
+        } catch (err) {
+            console.error(err);
+            setError(err.response?.data?.message || 'Register failed');
+
+        } finally {
+
+            setIsLoading(false)
+        }
+    };
+
+    return {
+        handleSubmit,
+        form,
+        isLoading,
+        error
+
+    };
+};
+
+export default useRegister;
