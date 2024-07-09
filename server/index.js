35147f19052701237b828580ac2add730d2f2574
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-10 01:20:50 +0300

    add cookieParser

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 const errorHandler = require('./middleware/errorHandler');
 const authRouter = require('./routes/authRoutes');
 
@@ -10,6 +11,7 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser);
 
 // routes
 app.use('/api/auth', authRouter);
