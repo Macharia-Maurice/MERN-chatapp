a8dd5a7c4622c122130fca762bf22d04621b8424
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-10 10:52:35 +0300

    correct error

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', authenticateToken, logout);
+router.post('/logout',  logout);
 router.post('/refresh-token', refreshToken);
 
 module.exports = router;
--- server/services/authService.js
+++ server/services/authService.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
-const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;  // Corrected to use REFRESH_TOKEN_SECRET
+const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
 
 // Function to generate access token
 exports.generateAccessToken = (user) => {
