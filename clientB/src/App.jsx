d9d3089c888cdbc2e7e8a805ea0044d025023401
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-10 19:40:24 +0300

    add routes

--- clientB/src/App.jsx
+++ clientB/src/App.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+
+      <Routes>
+
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+
+      </Routes>
+
+    </Router>
   )
 }
 
