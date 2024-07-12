0abbbf06029c7097ef089e1fb080ea178c6a7200
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-12 19:33:48 +0300

    remove error props

--- clientB/src/components/wrappers/CardWrapper.jsx
+++ clientB/src/components/wrappers/CardWrapper.jsx
@@ -4,26 +4,18 @@ import {
     CardTitle,
     CardDescription,
     CardContent,
-
 } from "../ui/card"
 
-const CardWrapper = ({ title, subtitle, error, children }) => {
+const CardWrapper = ({ title, subtitle, children }) => {
     return (
         <Card className='xl:w-1/4 md:w-1/2 shadow-md'>
-
             <CardHeader className="flex flex-col justify-center items-center">
-
                 <CardTitle className="text-xl font-semibold">{title}</CardTitle>
                 <CardDescription className="text-muted-foreground text-sm">{subtitle}</CardDescription>
-
             </CardHeader>
-
             <CardContent>
                 {children}
             </CardContent>
-
-            {error && <p className="text-red-500 text-center mt-2">{error}</p>}
-
         </Card>
     )
 }
