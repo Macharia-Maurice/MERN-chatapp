af9f327aed574571d97a649c0d5f33a2197be0a7
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-12 19:33:37 +0300

    remove error props

--- clientB/src/components/wrappers/AuthForm.jsx
+++ clientB/src/components/wrappers/AuthForm.jsx
@@ -4,7 +4,6 @@ import { Button } from "../ui/button";
 import { Form } from '../ui/form';
 
 const AuthForm = ({ title, subtitle, submitBtnTxt, hook, children }) => {
-
     const { handleSubmit, form, isLoading, error } = hook();
 
     return (
@@ -12,27 +11,20 @@ const AuthForm = ({ title, subtitle, submitBtnTxt, hook, children }) => {
             <CardWrapper
                 title={title}
                 subtitle={subtitle}
-                error={error}
             >
                 <Form {...form}>
-
-                    <form onSubmit={form.handleSubmit(handleSubmit)} className="flex flex-col gap-4">
-
-                        {React.Children.map(children, child => {
-                            return React.cloneElement(child, { form });
-                        })}
-
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+                        {React.Children.map(children, child => (
+                            React.cloneElement(child, { form })
+                        ))}
                         <Button className="w-full" type="submit" disabled={isLoading}>
-                            {isLoading ? submitBtnTxt == "Login" ? 'Logging in...' : "Registering..." : submitBtnTxt}
+                            {isLoading ? (submitBtnTxt === "Login" ? 'Logging in...' : 'Registering...') : submitBtnTxt}
                         </Button>
-
                     </form>
-
                 </Form>
-
             </CardWrapper>
         </div>
-    )
-}
+    );
+};
 
-export default AuthForm
+export default AuthForm;
