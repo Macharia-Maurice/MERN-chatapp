1af245199e734fb9cf4f442efcce0b72e3e84dca
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-13 13:44:32 +0300

    add logout page and protect dashboard route

--- clientB/src/App.jsx
+++ clientB/src/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import RequireAuth from './redux/features/auth/RequireAuth';
+import Logout from './pages/Logout';
+
 const App = () => {
   return (
     <Router>
-
       <Routes>
-
-        <Route path='/login' element={<Login/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/dashboard' element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        } />
       </Routes>
-
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
