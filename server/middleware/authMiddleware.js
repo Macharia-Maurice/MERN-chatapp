b8d117bbc2e8d56721d90cf881344a755ade61b1
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-14 07:03:59 +0300

    format using prettier

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,20 +1,24 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
 exports.authenticateToken = (req, res, next) => {
-    const token = req.cookies.accessToken;
+  const token = req.cookies.accessToken;
 
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized', message: 'No token found' });
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: "Unauthorized", message: "No token found" });
+  }
+
+  jwt.verify(token, accessTokenSecret, (err, user) => {
+    if (err) {
+      console.error("JWT verification error:", err.message);
+      return res
+        .status(401)
+        .json({ error: "Unauthorized", message: "Invalid or expired token" });
     }
 
-    jwt.verify(token, accessTokenSecret, (err, user) => {
-        if (err) {
-            console.error('JWT verification error:', err.message);
-            return res.status(401).json({ error: 'Unauthorized', message: 'Invalid or expired token' });
-        }
-        
-        req.user = user;
-        next();
-    });
+    req.user = user;
+    next();
+  });
 };
