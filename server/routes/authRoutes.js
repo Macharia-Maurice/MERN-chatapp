add170a580bb8d01cfb0e2a4edd18f992455e5b7
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-14 07:06:48 +0300

    format using prettier

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const { register, login, logout, refreshToken } = require('../controllers/authControler');
-const { authenticateToken } = require('../middleware/authMiddleware')
+const express = require("express");
+const {
+  register,
+  login,
+  logout,
+  refreshToken,
+} = require("../controllers/authControler");
+const { authenticateToken } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', authenticateToken, logout);
-router.post('/refresh-token', refreshToken);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", authenticateToken, logout);
+router.post("/refresh-token", refreshToken);
 
 module.exports = router;
