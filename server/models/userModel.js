7902ce6c43057e98f79e3ce56558472e61713d13
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-14 07:35:16 +0300

    format with prettier

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,16 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
+// Define the User schema
 const UserSchema = new Schema(
   {
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   {
-    timestamps: true,
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
   }
 );
 
+// Create and export the User model
 module.exports = model("User", UserSchema);
