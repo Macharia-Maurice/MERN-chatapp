bb923e28c8abc90d0eaa7138b2c34ad7ff436b5c
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-14 07:06:12 +0300

    add timeStamp to user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const model = mongoose.model;
 
-const UserSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = model("User", UserSchema);
