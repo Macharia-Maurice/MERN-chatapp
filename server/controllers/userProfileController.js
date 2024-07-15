abc192753b97160ccc815b5c009d614c4bb8da60
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-15 23:13:00 +0300

    get user profile for current logged in user

--- server/controllers/userProfileController.js
+++ server/controllers/userProfileController.js
@@ -1,66 +1,66 @@
 const UserProfile = require("../models/userProfileModel");
 const createError = require("../utils/appError");
 
-// List all user profiles with pagination
-exports.listAllProfiles = async (req, res, next) => {
-  try {
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 10;
-    const skip = (page - 1) * limit;
-
-    // Count total profiles
-    const total = await UserProfile.countDocuments();
-
-    // Find profiles
-    const profiles = await UserProfile.find()
-      .populate("user", "username email") // Populate only necessary fields
-      .select("-__v") // Exclude the version key
-      .skip(skip)
-      .limit(limit)
-      .sort({ createdAt: -1 }); // Sort by creation date, newest first
-
-    // Calculate total pages
-    const totalPages = Math.ceil(total / limit);
-
-    res.status(200).json({
-      status: "success",
-      data: {
-        profiles,
-        currentPage: page,
-        totalPages,
-        totalProfiles: total,
-      },
-    });
-  } catch (err) {
-    console.error("Error listing profiles:", err);
-    next(new createError("Internal server error", 500));
-  }
-};
-
-
-// // for current logged in user
-// exports.getUserProfile = async (req, res, next) => {
-//   const userId = req.user.id;
-//   console.log("Authenticated User ID:", userId);
+// // List all user profiles with pagination
+// exports.listAllProfiles = async (req, res, next) => {
 //   try {
-//     const userProfile = await UserProfile.findOne({ user: userId }).populate(
-//       "user"
-//     );
+//     // Pagination
+//     const page = parseInt(req.query.page, 10) || 1;
+//     const limit = parseInt(req.query.limit, 10) || 10;
+//     const skip = (page - 1) * limit;
 
-//     if (!userProfile) {
-//       return next(new createError("User profile not found", 404));
-//     }
+//     // Count total profiles
+//     const total = await UserProfile.countDocuments();
+
+//     // Find profiles
+//     const profiles = await UserProfile.find()
+//       .populate("user", "username email") // Populate only necessary fields
+//       .select("-__v") // Exclude the version key
+//       .skip(skip)
+//       .limit(limit)
+//       .sort({ createdAt: -1 }); // Sort by creation date, newest first
+
+//     // Calculate total pages
+//     const totalPages = Math.ceil(total / limit);
 
 //     res.status(200).json({
 //       status: "success",
-//       userProfile,
+//       data: {
+//         profiles,
+//         currentPage: page,
+//         totalPages,
+//         totalProfiles: total,
+//       },
 //     });
 //   } catch (err) {
-//     console.error("Error getting user profile:", err);
+//     console.error("Error listing profiles:", err);
 //     next(new createError("Internal server error", 500));
 //   }
 // };
+
+
+// for current logged in user
+exports.getUserProfile = async (req, res, next) => {
+  const userId = req.user.id;
+  console.log("Authenticated User ID:", userId);
+  try {
+    const userProfile = await UserProfile.findOne({ user: userId }).populate(
+      "user"
+    );
+
+    if (!userProfile) {
+      return next(new createError("User profile not found", 404));
+    }
+
+    res.status(200).json({
+      status: "success",
+      userProfile,
+    });
+  } catch (err) {
+    console.error("Error getting user profile:", err);
+    next(new createError("Internal server error", 500));
+  }
+};
 
 
 // // update the userProfile
