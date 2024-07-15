32651061920274696b2e0d56a3f9bce804083ea3
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-15 23:13:47 +0300

    fix use user.id instead of user._id

--- server/controllers/userProfileController.js
+++ server/controllers/userProfileController.js
@@ -1,42 +1,42 @@
 const UserProfile = require("../models/userProfileModel");
 const createError = require("../utils/appError");
 
-// // List all user profiles with pagination
-// exports.listAllProfiles = async (req, res, next) => {
-//   try {
-//     // Pagination
-//     const page = parseInt(req.query.page, 10) || 1;
-//     const limit = parseInt(req.query.limit, 10) || 10;
-//     const skip = (page - 1) * limit;
+// List all user profiles with pagination
+exports.listAllProfiles = async (req, res, next) => {
+  try {
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const skip = (page - 1) * limit;
 
-//     // Count total profiles
-//     const total = await UserProfile.countDocuments();
+    // Count total profiles
+    const total = await UserProfile.countDocuments();
 
-//     // Find profiles
-//     const profiles = await UserProfile.find()
-//       .populate("user", "username email") // Populate only necessary fields
-//       .select("-__v") // Exclude the version key
-//       .skip(skip)
-//       .limit(limit)
-//       .sort({ createdAt: -1 }); // Sort by creation date, newest first
+    // Find profiles
+    const profiles = await UserProfile.find()
+      .populate("user", "username email") // Populate only necessary fields
+      .select("-__v") // Exclude the version key
+      .skip(skip)
+      .limit(limit)
+      .sort({ createdAt: -1 }); // Sort by creation date, newest first
 
-//     // Calculate total pages
-//     const totalPages = Math.ceil(total / limit);
+    // Calculate total pages
+    const totalPages = Math.ceil(total / limit);
 
-//     res.status(200).json({
-//       status: "success",
-//       data: {
-//         profiles,
-//         currentPage: page,
-//         totalPages,
-//         totalProfiles: total,
-//       },
-//     });
-//   } catch (err) {
-//     console.error("Error listing profiles:", err);
-//     next(new createError("Internal server error", 500));
-//   }
-// };
+    res.status(200).json({
+      status: "success",
+      data: {
+        profiles,
+        currentPage: page,
+        totalPages,
+        totalProfiles: total,
+      },
+    });
+  } catch (err) {
+    console.error("Error listing profiles:", err);
+    next(new createError("Internal server error", 500));
+  }
+};
 
 
 // for current logged in user
@@ -63,32 +63,32 @@ exports.getUserProfile = async (req, res, next) => {
 };
 
 
-// // update the userProfile
-// exports.updateUserProfile = async (req, res, next) => {
-//   const userId = req.user.id;
-//   console.log("Authenticated User ID:", userId);
-//   const { profilePicture } = req.body;
+// update the userProfile
+exports.updateUserProfile = async (req, res, next) => {
+  const userId = req.user.id;
+  console.log("Authenticated User ID:", userId);
+  const { profilePicture } = req.body;
 
-//   try {
-//     const userProfile = await UserProfile.findOne({ user: userId });
+  try {
+    const userProfile = await UserProfile.findOne({ user: userId });
 
-//     if (!userProfile) {
-//       return next(new createError("user profile not found", 404));
-//     }
+    if (!userProfile) {
+      return next(new createError("user profile not found", 404));
+    }
 
-//     if (profilePicture) {
-//       userProfile.profilePicture = profilePicture;
-//     }
+    if (profilePicture) {
+      userProfile.profilePicture = profilePicture;
+    }
 
-//     await userProfile.save();
+    await userProfile.save();
 
-//     res.status(200).json({
-//       status: "success",
-//       message: "User profile updated successfully",
-//       userProfile,
-//     });
-//   } catch (err) {
-//     console.error("Update userProfile error:", err);
-//     next(new createError("internal server error", 500));
-//   }
-// };
+    res.status(200).json({
+      status: "success",
+      message: "User profile updated successfully",
+      userProfile,
+    });
+  } catch (err) {
+    console.error("Update userProfile error:", err);
+    next(new createError("internal server error", 500));
+  }
+};
