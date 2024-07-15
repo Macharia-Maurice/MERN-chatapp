f3d6380d8bce0a9aeee8893eb7dd7d33c347df58
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-16 01:15:23 +0300

    add route to list all profiles

--- server/routes/userprofileRoutes.js
+++ server/routes/userprofileRoutes.js
@@ -3,11 +3,14 @@ const {
   authenticateToken,
 } = require("../middleware/accessTokenAuthMiddleware");
 const {
+  listAllProfiles,
   getUserProfile,
   updateUserProfile,
 } = require("../controllers/userProfileController");
 const router = express();
 
+
+router.get("/", authenticateToken, listAllProfiles);
 // current logged in user
 router.get("/me", authenticateToken, getUserProfile);
 router.patch("/update", authenticateToken, updateUserProfile);
