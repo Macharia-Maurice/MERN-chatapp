486c4e2d1754aadbe09811a0e2b5a925fbfe59bf
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-16 12:16:08 +0300

    edited

--- server/middleware/accessTokenAuthMiddleware.js
+++ server/middleware/accessTokenAuthMiddleware.js
@@ -2,24 +2,25 @@ const jwt = require("jsonwebtoken");
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
 exports.authenticateToken = (req, res, next) => {
-  const token = req.cookies.accessToken;
+	const token = req.cookies.accessToken;
 
-  if (!token) {
-    return res
-      .status(401)
-      .json({ error: "Unauthorized", message: "No token found" });
-  }
+	if (!token) {
+		return res
+			.status(401)
+			.json({ error: "Unauthorized", message: "No token found" });
+	}
 
-  jwt.verify(token, accessTokenSecret, (err, user) => {
-    if (err) {
-      console.error("JWT verification error:", err.message);
-      return res
-        .status(401)
-        .json({ error: "Unauthorized", message: "Invalid or expired token" });
-    }
+	jwt.verify(token, accessTokenSecret, (err, user) => {
+		if (err) {
+			console.error("JWT verification error:", err.message);
+			return res.status(401).json({
+				error: "Unauthorized",
+				message: "Invalid or expired token",
+			});
+		}
 
-    req.user = user;
-    console.log("Authenticated user:", user);
-    next();
-  });
+		req.user = user;
+		console.log("Authenticated user:", user);
+		next();
+	});
 };
