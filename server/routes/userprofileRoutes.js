62719d99ba4731c8edd0e5377737362239c45636
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-16 23:39:43 +0300

    update profile routes

--- server/routes/userprofileRoutes.js
+++ server/routes/userprofileRoutes.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const {
-  authenticateToken,
+	authenticateToken,
 } = require("../middleware/accessTokenAuthMiddleware");
 const {
-  listAllProfiles,
-  getUserProfile,
-  updateUserProfile,
+	listAllProfiles,
+	getUserProfile,
+	updateUserProfile,
 } = require("../controllers/userProfileController");
 const router = express();
 
+// router.get("/", authenticateToken, listAllProfiles);
+// router.patch("/", authenticateToken, updateUserProfile);
 
-router.get("/", authenticateToken, listAllProfiles);
+router
+  .route("/")
+  .get(authenticateToken, listAllProfiles)
+  .patch(authenticateToken, updateUserProfile);
+
 // current logged in user
 router.get("/me", authenticateToken, getUserProfile);
-router.patch("/update", authenticateToken, updateUserProfile);
 
 module.exports = router;
