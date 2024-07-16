1d35d4372dd38d8b6bbf623b7fc11524069ee35e
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-17 00:45:32 +0300

    update forminput to handle files

--- client/src/components/wrappers/FormInput.jsx
+++ client/src/components/wrappers/FormInput.jsx
@@ -1,34 +1,37 @@
+// FormInput.js
 import {
     FormItem,
     FormLabel,
     FormMessage,
     FormField,
     FormControl
-} from '../ui/form'
-import { Input } from "../ui/input";
-
-const FormInput = ({ name, label, type, form }) => {
+  } from '../ui/form';
+  import { Input } from "../ui/input";
+  
+  const FormInput = ({ name, label, type, form }) => {
     return (
-        <FormField
-
-            control={form.control}
-            name={name}
-
-            render={({ field }) => (
-
-                <FormItem>
-                    <FormLabel>{label}</FormLabel>
-
-                    <FormControl>
-                        <Input {...field} type={type} />
-                    </FormControl>
-
-                    <FormMessage />
-
-                </FormItem>
-            )}
-        />
-    )
-}
-
-export default FormInput
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{label}</FormLabel>
+            <FormControl>
+              {type === "file" ? (
+                <input
+                  type="file"
+                  onChange={(e) => field.onChange(e.target.files)}
+                />
+              ) : (
+                <Input {...field} type={type} />
+              )}
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    );
+  };
+  
+  export default FormInput;
+  
