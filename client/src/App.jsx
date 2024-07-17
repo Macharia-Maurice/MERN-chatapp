679c725415b20cb1dc097190e58d5619a0fb827a
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-17 19:43:57 +0300

    add profile route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import RequireAuth from './redux/features/auth/RequireAuth';
-import Logout from './pages/Logout';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import RequireAuth from "./redux/features/auth/RequireAuth";
+import Logout from "./pages/Logout";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/logout' element={<Logout />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={
-          <RequireAuth>
-            <Dashboard />
-          </RequireAuth>
-        } />
-      </Routes>
-    </Router>
-  );
-}
+	return (
+		<Router>
+			<Routes>
+				<Route
+					path="/login"
+					element={<Login />}
+				/>
+				<Route
+					path="/logout"
+					element={<Logout />}
+				/>
+				<Route
+					path="/register"
+					element={<Register />}
+				/>
+				<Route
+					path="/dashboard"
+					element={
+						<RequireAuth>
+							<Dashboard />
+						</RequireAuth>
+					}
+				/>
+				<Route
+					path="/profile"
+					element={
+						<RequireAuth>
+							<Profile />
+						</RequireAuth>
+					}
+				/>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
