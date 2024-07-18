7ab999537ae50ce36acf44d6e9e3a5283285c6ed
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-18 22:40:30 +0300

    Add CometChatUserWithMessges component

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { CometChatUsersWithMessages } from "@cometchat/chat-uikit-react";
+
+
+const Chat = () => {
+  return (
+    <CometChatUsersWithMessages/>
+  )
+}
+
+export default Chat
