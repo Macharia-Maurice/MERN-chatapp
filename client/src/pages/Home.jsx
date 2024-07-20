066f953126fd28be9829af2e377554758eb45388
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-20 09:51:33 +0300

    Add SideBar and Message page to HomePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import SideBar from "@/components/common/SideBar";
+import MessagePage from "@/components/common/MessagePage";
 
 const Home = () => {
 	return (
 		<div className="min-h-screen bg-gray-300 flex">
-			<SideBar />
-			messagePage
+			<div className="w-full">
+				<SideBar />
+			</div>
+			<div className="w-full">
+				<MessagePage />
+			</div>
 		</div>
 	);
 };
