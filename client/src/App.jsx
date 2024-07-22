214e7e4d596427240243cf8b70fecf2d0f23365e
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-22 18:02:42 +0300

    rename component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
+import Home from "./pages/HomePage";
 import RequireAuth from "./redux/features/auth/RequireAuth";
 import Logout from "./pages/auth/Logout";
-import Profile from "./pages/Profile";
+import Profile from "./pages/ProfileViewPage";
 
 const App = () => {
 	return (
