13e4902d71edf98d447f8d72bc0cc8ac7d25092a
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-25 22:51:59 +0300

    refactor: Update userprofileRoutes to include getMyProfile endpoint and improve code formatting

--- server/routes/userprofileRoutes.js
+++ server/routes/userprofileRoutes.js
@@ -1,37 +1,50 @@
 const express = require("express");
-const { authenticateToken } = require("../middleware/accessTokenAuthMiddleware");
-const { listAllProfiles, getUserProfile, updateUserProfile, updateUserProfilePicture } = require("../controllers/userProfileController");
+const {
+	authenticateToken,
+} = require("../middleware/accessTokenAuthMiddleware");
+const {
+	listAllProfiles,
+    getMyProfile,
+	getUserProfile,
+	updateUserProfile,
+	updateUserProfilePicture,
+} = require("../controllers/userProfileController");
 const multer = require("multer");
 const router = express.Router();
-const path = require('path')
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination:(req, file, cb)=>{
-        cb(null, 'Images')
-    },
-    filename:(req, file, cb)=>{
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
+	destination: (req, file, cb) => {
+		cb(null, "Images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, Date.now() + path.extname(file.originalname));
+	},
+});
 
-const upload = multer({ storage: storage})
-
+const upload = multer({ storage: storage });
 
 //protect routes
-router.use(authenticateToken)
+router.use(authenticateToken);
 
-router.route('/')
-    // List all profiles
-    .get(listAllProfiles)
-    // Update user profile
-    .patch(updateUserProfile);
+router
+	.route("/")
+	// List all profiles
+	.get(listAllProfiles)
+	// Update user profile
+	.patch(updateUserProfile);
 
 // Update user profile picture
-router.patch("/picture", upload.single("profilePicture"), updateUserProfilePicture);
+router.patch(
+	"/picture",
+	upload.single("profilePicture"),
+	updateUserProfilePicture
+);
 
 // Get current logged in user profile
-router.get("/me", getUserProfile);
+router.get("/me", getMyProfile);
 
-
+// Get a user profile given the id
+router.get("/:profile_id", getUserProfile);
 
 module.exports = router;
