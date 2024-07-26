f20a7a729a8bde46259aeffffe6606371d910a1c
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-26 19:44:50 +0300

    update the parameters

--- client/src/redux/features/messages/messageApiSlice.js
+++ client/src/redux/features/messages/messageApiSlice.js
@@ -2,29 +2,30 @@
 import { baseApi } from "@/redux/baseApiSlice";
 
 const messageApiSlice = baseApi.injectEndpoints({
-    overrideExisting: true,
-    endpoints: (builder) => ({
-        createMessage: builder.mutation({
-            query: ({ chat_id, message }) => ({
-                url: `/messages/${chat_id}`,
-                method: "POST",
-                body: { message },
-            }),
-        }),
-        getAllMessages: builder.query({
-            query: (chat_id) => `/messages/${chat_id}`,
-        }),
-        seenBy: builder.mutation({
-            query: (message_id) => ({
-                url: `/messages/${message_id}/seen`,
-                method: "PATCH",
-            }),
-        }),
-    }),
+	overrideExisting: true,
+	endpoints: (builder) => ({
+		createMessage: builder.mutation({
+			query: ({ chat_id, text, replyTo }) => ({
+				url: `/messages/${chat_id}`,
+				method: "POST",
+				body: { text, replyTo },
+			}),
+		}),
+
+		getAllMessages: builder.query({
+			query: (chat_id) => `/messages/${chat_id}`,
+		}),
+		seenBy: builder.mutation({
+			query: (message_id) => ({
+				url: `/messages/${message_id}/seen`,
+				method: "PATCH",
+			}),
+		}),
+	}),
 });
 
 export const {
-    useCreateMessageMutation,
-    useGetAllMessagesQuery,
-    useSeenByMutation
+	useCreateMessageMutation,
+	useGetAllMessagesQuery,
+	useSeenByMutation,
 } = messageApiSlice;
