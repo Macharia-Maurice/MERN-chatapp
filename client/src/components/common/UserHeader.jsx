e75f0e5738c0a0b1951762609f5ea164ed86dee0
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-27 10:14:54 +0300

    od onClick eventlistener to new chat button to list all profiles

--- client/src/components/common/UserHeader.jsx
+++ client/src/components/common/UserHeader.jsx
@@ -3,16 +3,16 @@ import { useProfileMeQuery } from '@/redux/features/profiles/profileApiSlice';
 import { Link } from 'react-router-dom';
 import { Avatar } from 'react-chat-elements';
 
-const UserHeader = () => {
+const UserHeader = ({ onNewChatClick }) => {
     const { data, error, isLoading } = useProfileMeQuery();
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error loading profile</div>;
+    if (isLoading) return <div className="text-center text-gray-500">Loading profile...</div>;
+    if (error) return <div className="text-center text-red-500">Error loading profile</div>;
 
     const { profilePicture } = data.userProfile;
 
     return (
-        <div className="flex items-center justify-between p-4 mb-4 border-b border-gray-300 bg-white shadow-sm">
+        <div className="flex items-center justify-between p-4 mb-4 border-b border-gray-300 bg-white shadow-md">
             <Link to="/profile">
                 <Avatar
                     src={`http://localhost:2000/${profilePicture}`}
@@ -22,14 +22,12 @@ const UserHeader = () => {
                     className="cursor-pointer hover:opacity-80 transition-opacity duration-300"
                 />
             </Link>
-            <div className="flex items-center space-x-4">
-                <Link to="#">
-                    <button className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300">
-                        New Chat
-                    </button>
-                </Link>
-
-            </div>
+            <button
+                onClick={onNewChatClick}
+                className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300"
+            >
+                New Chat
+            </button>
         </div>
     );
 };
