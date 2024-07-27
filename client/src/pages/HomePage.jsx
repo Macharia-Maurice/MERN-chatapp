2312c0448e197a7eb6c990aed056d5f55cdf79ce
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-27 18:53:17 +0300

    retrieve current logged in user profile (to provide to messagepage)

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,6 +6,7 @@ import SideBar from "@/components/common/SideBar";
 import MessagePage from "@/components/common/MessagePage";
 import { useGetAllMessagesQuery } from "@/redux/features/messages/messageApiSlice";
 import ProfilesList from "@/components/common/ProfilesList";
+import { useProfileMeQuery } from "@/redux/features/profiles/profileApiSlice";
 
 const Home = () => {
     const [selectedChat, setSelectedChat] = useState(null);
@@ -16,10 +17,14 @@ const Home = () => {
     const { data: messages, isFetching: isFetchingMessages } = useGetAllMessagesQuery(selectedChat?._id, {
         skip: !selectedChat
     });
+    const { data: userProfile, isLoading: isLoadingProfile, isError: isErrorProfile } = useProfileMeQuery();
 
-    if (isLoadingChats || isLoadingProfiles) return <div>Loading...</div>;
+    if (isLoadingChats || isLoadingProfiles || isLoadingProfile) return <div>Loading...</div>;
     if (isErrorChats) return <div>Error loading chats.</div>;
     if (isErrorProfiles) return <div>Error loading profiles.</div>;
+    if (isErrorProfile) return <div>Error loading user profile.</div>;
+
+    const currentUserId = userProfile?.userProfile?._id;
 
     return (
         <div className="flex h-screen bg-gray-100">
@@ -46,7 +51,7 @@ const Home = () => {
                     isFetchingMessages ? (
                         <div>Loading messages...</div>
                     ) : (
-                        <MessagePage selectedChat={selectedChat} messages={messages?.data?.messages || []} />
+                        <MessagePage selectedChat={selectedChat} messages={messages?.data?.messages || []} currentUserId={currentUserId}/>
                     )
                 ) : (
                     <div>Select a chat to view messages</div>
