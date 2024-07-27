cb238df030be7b452741a1ba7b9ad47c3b7f2910
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-27 19:56:34 +0300

    refactor: Remove position field from messageModel

--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -31,10 +31,6 @@ const messageSchema = new Schema(
 			ref: "Message",
 			default: null,
 		},
-		position:{
-			type:String,
-			default:"",
-		},
 	},
 	{ timestamps: true } // Automatically manage createdAt and updatedAt fields
 );
