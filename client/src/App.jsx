3be4deeea26416c3ef1388da73b8ef16afc625a4
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-07-28 20:34:54 +0300

    emit profileOnline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import io from "socket.io-client";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/HomePage";
@@ -7,59 +9,38 @@ import Logout from "./pages/auth/Logout";
 import MyProfileView from "./pages/MyProfileViewPage";
 import UserProfileView from "./pages/UserProfileViewPage";
 import ProfilesList from "./components/common/ProfilesList";
+import { useProfileMeQuery } from "@/redux/features/profiles/profileApiSlice";
+
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 const App = () => {
-	return (
-		<Router>
-			<Routes>
-				<Route
-					path="/login"
-					element={<Login />}
-				/>
-				<Route
-					path="/logout"
-					element={<Logout />}
-				/>
-				<Route
-					path="/register"
-					element={<Register />}
-				/>
-				<Route
-					path="/home"
-					element={
-						<RequireAuth>
-							<Home />
-						</RequireAuth>
-					}
-				/>
-				<Route
-					path="/profile"
-					element={
-						<RequireAuth>
-							<MyProfileView />
-						</RequireAuth>
-					}
-				/>
-				<Route
-					path="/profile/:profile_id"
-					element={
-						<RequireAuth>
-							<UserProfileView />
-						</RequireAuth>
-					}
-				/>
-				<Route
-					path="/profile_list"
-					element={
-						<RequireAuth>
-							<ProfilesList />
-						</RequireAuth>
-					}
-				/>
+    const { data: meData } = useProfileMeQuery();
 
-			</Routes>
-		</Router>
-	);
+    useEffect(() => {
+        if (meData?.userProfile) {
+            socket.emit("profileOnline", meData.userProfile._id);
+        }
+
+        return () => {
+            if (meData?.userProfile) {
+                socket.emit("profileLogout", meData.userProfile._id);
+            }
+        };
+    }, [meData]);
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/logout" element={<Logout />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/home" element={<RequireAuth><Home /></RequireAuth>} />
+                <Route path="/profile" element={<RequireAuth><MyProfileView /></RequireAuth>} />
+                <Route path="/profile/:profile_id" element={<RequireAuth><UserProfileView /></RequireAuth>} />
+                <Route path="/profile_list" element={<RequireAuth><ProfilesList /></RequireAuth>} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
